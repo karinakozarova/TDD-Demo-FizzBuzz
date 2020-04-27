3144e85312503c2d81cc4ad7f81204b6deaee028
Karina Kozarova <karinakozarova1303@gmail.com>
2020-04-27 13:45:09 +0200

    process number 1

---
+++ FizzBuzzTDD/FizzBuzzTDD/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzTDD
+{
+    public class FizzBuzz
+    {
+        public string ProcessNumber(int value)
+        {
+            return 1.ToString();
+        }
+    }
+}
---
+++ FizzBuzzTDD/FizzBuzzUnitTests/FizzBuzzClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzzTDD;
+
+namespace FizzBuzzUnitTests
+{
+    [TestClass]
+    public class FizzBuzzClass
+    {
+        [TestMethod]
+        public void ShouldProcessANumber()
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            String output = fizzBuzz.ProcessNumber(1);
+            Assert.AreEqual(output, "1");
+        }
+    }
+}
